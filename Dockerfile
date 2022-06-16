403f9052c6b5318e0fd76e19129637b053b058dd
Fedor Popov <popovf@netcracker.com>
2022-06-16 17:05:24 -0400

    change jar to war in build.gradle and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
-FROM openjdk:8-jdk-alpine
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
-
-EXPOSE 8080
+FROM java
+ADD /target/*.war vk_media.war
+ENTRYPOINT ["java", "-jar", "/vk_media.war"]
--- src/main/java/com/vk_media/vkmedia/VkMediaApplication.java
+++ src/main/java/com/vk_media/vkmedia/VkMediaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = "com.vk_media.vkmedia")
+@SpringBootApplication
 public class VkMediaApplication  extends SpringBootServletInitializer {
 
 	@Override
