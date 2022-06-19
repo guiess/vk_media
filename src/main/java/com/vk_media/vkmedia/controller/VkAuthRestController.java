8cb4ccfba6714f3e31f7fa84c0de8da53a62c2a0
Fedor Popov <popovf@netcracker.com>
2022-06-19 00:00:07 -0400

    add tesseract text recognition function unfortunately works very poor for pictures with text

--- src/main/java/com/vk_media/vkmedia/controller/VkAuthRestController.java
+++ src/main/java/com/vk_media/vkmedia/controller/VkAuthRestController.java
@@ -4,6 +4,9 @@ import com.vk_media.vkmedia.dto.PhotoWithImage;
 import com.vk_media.vkmedia.repository.MongoDBRepository;
 import com.vk_media.vkmedia.service.VkAuthService;
 import com.vk_media.vkmedia.service.VkPhotoService;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.util.LoadLibs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 @RestController
@@ -34,13 +42,23 @@ public class VkAuthRestController {
         return new ModelAndView("redirect:/");
     }
 
-    @Autowired
-    MongoDBRepository mongoDBRepository;
-    @GetMapping("/test")
-    public List<PhotoWithImage> findPhoto() {
-        System.out.println("!!! mongo count "+mongoDBRepository.count());
 
-        //return mongoDBRepository.findAll();
-        return mongoDBRepository.findByTagsRegex("t");
+    @GetMapping("/recognize")
+    public String getTextFromImage() throws IOException {
+        String imageURL = "https://sun9-8.userapi.com/impf/pdRtd00XMhtcIeRAWjthZSiCPEKkas1_w419jA/qWybBC2nH6U.jpg?size=604x383&quality=96&sign=e9e35bc840f4ba3c2f277814c982a1ec&type=album";
+        String result = "no result";
+        //File tessDataFolder = LoadLibs.extractTessResources("tessdata");
+        URL url = new URL(imageURL);
+        BufferedImage img = ImageIO.read(url);
+        Tesseract tesseract = new Tesseract();
+        tesseract.setLanguage("rus");
+        tesseract.setDatapath("tessdata");
+        try {
+            result = tesseract.doOCR(img);
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
+
 }
